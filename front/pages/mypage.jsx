7fe88ded9a4ff3f26e797b625f1d3404923b6234
Sangyub <godtttever@naver.com>
2021-10-01 10:22:33 +0900

    됐나??

--- front/pages/mypage.jsx
+++ front/pages/mypage.jsx
@@ -1,18 +1,37 @@
 import React from 'react'
 import Styled from 'styled-components'
+import Navigation from '../Layouts/Navigation'
 
 const Mypage = () => {
-    return (
+    return ( 
         <>
-            {/* 상엽 part
-            <div> hello </div>   가 있으면 
-            
-            css 적용하게 되면
-            <Example> hello </Example>
-            로 하면 됩니다.
-
-            여기서 부터는 html 문법으로 작성해주세요
-            */}
+        <Navigation/>
+        <Mypageatall>
+              <div>
+                  <Mypagetop>
+                  <div>
+                      마이페이지
+                  </div>
+                  </Mypagetop>
+                  <MypageNav>
+                  <div>
+                        <div>잔고</div>
+                  </div>
+                  </MypageNav>
+                  <Myhistory>
+                  <ul>
+                        <li>체결시간</li>
+                        <li>코인명</li>
+                        <li>종류</li>
+                        <li>거래수량</li>
+                        <li>거래단가</li>
+                        <li>거래금액</li>
+                        <li>주문시간</li>
+                  </ul>
+                  </Myhistory>
+              </div>
+         
+        </Mypageatall>
         </>
     )
 }
@@ -29,3 +48,32 @@ const Example(변수명 앞 글자 대문자) = Styled.div`
 
 `
 */
+
+const Mypageatall = Styled.div`
+    width:100%;
+    height:100%;
+`
+
+const Mypagetop = Styled.div`
+    display: flex;
+    justify-content: center;
+    margin-bottom: 1rem;
+    margin: 0;
+    font-size: 36px;
+`
+
+const MypageNav = Styled.div`
+    width : 25%;
+    height: 200px;
+    margin : 10px 20px;
+`
+
+const Myhistory = Styled.ul`
+        margin-left : 20px;
+        & li {
+            float:left;
+            width:200px;
+            background-color: #f9fafc;
+            color: #666;
+        }
+`
