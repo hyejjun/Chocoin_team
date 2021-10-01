7975344febefaebf32c284dc3f3f3535729dda44
algml <algml9603@gmail.com>
2021-10-01 17:12:02 +0900

    test

--- back/db.js
+++ back/db.js
@@ -2,13 +2,13 @@ const mysql = require('mysql')
 const config = require('./db_config.json')
 const pool = mysql.createPool(config)
 
-function get_data(req,res,querysyn){
+function get_data(req,res,querysyn,msg_suc,msg_fail){
     pool.getConnection((err,connection)=>{
         if(err) throw err;
         connection.query(querysyn,function(error,results,fields){
             if(error) throw error;
             if(results==undefined){
-                res.json({'msg':'db connection fail'})
+                res.json({'msg':msgtext})
             }else{
                 res.json({'msg':'db suc',results})
             }
@@ -17,7 +17,7 @@ function get_data(req,res,querysyn){
     })
 }
 
-function send_data(req,res,querysyn){
+function send_data(req,res,querysyn,msg_suc,msg_fail){
     pool.getConnection((err,connection)=>{
         if(err) throw err;
         connection.query(querysyn,function(error,results,fields){
@@ -25,7 +25,7 @@ function send_data(req,res,querysyn){
             if(results==undefined){
                 res.json({'msg':'db connection fail'})
             }else{
-                res.json({'msg':'db connection success'})
+                res.json({'msg':msgtext})
             }
             connection.release();
         })
