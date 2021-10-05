f673f3cd7ec0fefd0f96d3d335d52a0c82bfe213
heini <melme900@gmail.com>
2021-10-05 15:26:32 +0900

    logout css

--- front/Layouts/Navigation.jsx
+++ front/Layouts/Navigation.jsx
@@ -1,11 +1,8 @@
 import Link from 'next/link'
 import Styled from 'styled-components'
 import { useDispatch,useSelector } from 'react-redux'
-import { useEffect } from 'react'
-// import { logout } from '../../back/middleware/auth'
 import { user_logout } from '../reducers/user'
 import Logout from '../pages/user/logout'
-import Router from "next/router";
 
 const Navigation = () => {
 
@@ -20,7 +17,7 @@ const Navigation = () => {
             <Gnb>
                 <li className="logo"><Link href='/'><a>chocoin</a></Link></li>
                 <li><Link href='/'><a>거래소</a></Link></li>
-                <li><Link href='/user/join'><a>회원가입</a></Link></li>
+                {/* <li><Link href='/user/join'><a>회원가입</a></Link></li> */}
                 <li><Link href='/mypage'><a>내 정보</a></Link></li>
                 <li><Link href='/coininfo'><a>코인 정보</a></Link></li>
                 <li className="logout_btn" onClick={handleLogout}><Logout/></li>
