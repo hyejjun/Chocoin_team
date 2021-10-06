bdbbfe7794e21a246a73fe93e6e5d1484bdbb08a
heini <melme900@gmail.com>
2021-10-06 11:24:17 +0900

    middleware delete

--- back/routes/main/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('./controller');
-const auth = require('../../middleware/auth');
-
-router.get('/logined',controller.main);
-router.get('/',controller.first_main);
-
-module.exports = router;
